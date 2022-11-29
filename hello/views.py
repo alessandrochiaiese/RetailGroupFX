f6f0fba9bc4a58d3448259695260247ab1a4cda2
alessandrochiaiese <alessandro.chiaiese@libero.it>
2022-11-29 17:57:29 +0100

    .

--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,33 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def facts(request):
+    return render(request, "facts.html")
+
+def feature(request):
+    return render(request, "feature.html")
+
+def project(request):
+    return render(request, "project.html")
+
+def service(request):
+    return render(request, "service.html")
+    
+def team(request):
+    return render(request, "team.html")
+
+def terms(request):
+    return render(request, "terms.html")
+
+def testimonial(request):
+    return render(request, "testimonial.html")
+
 
 def db(request):
 
